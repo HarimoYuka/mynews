f1d44a1fdfd7967c9534b41d95a15900d2e96169
Yuka Harimoto <yuka91719@gmail.com>
2020-03-27 19:12:06 +0000

    13 get post

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -12,6 +12,13 @@
     </body>
 </html>
 
+<!--//13-4.【応用】 resources/views/admin/profile/create.blade.php を編集して、
+//氏名(name)、性別(gender)、趣味(hobby)、自己紹介欄(introduction)を
+//入力するフォームを作成してください。また、formの送信先(<form action=”この部分”>)を、 
+//Admin\ProfileController の create Action に指定してください。
+//(ヒント: resources/views/admin/news/create.blade.php)
+-->
+
 @extends('layouts.profile')
 @section('title', 'プロフィールの作成')
 @section('content')
@@ -19,6 +26,43 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}"
+                method="post" enctype="multipart/form-data">
+                    
+                       @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label></label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label></label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="gender" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hoby" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduce" rows="15">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,7 +46,6 @@
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
-                            
                         <!-- Authentication Links -->
                         {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                         @guest
